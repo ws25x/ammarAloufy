3ed37ba48350d53d4b1ba4cfd9b3d15238e6d3e3
ammar aloufy <ammar54222@gmail.com>
2025-04-26 21:50:45 +0300

    Delete soraaApp directory

--- soraaApp/android/app/src/main/kotlin/com/example/as_project/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.as_project
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- soraaApp/lib/widget/appColors.dart
+++
@@ -1,5 +0,0 @@
-import 'dart:ui';
-
-final Color greenFlag = Color.fromARGB(255, 49, 115, 51);
-// final Color Primerybutton = Color(0x8bc34a);
-final Color sec = Color.fromARGB(255, 248, 245, 233);
